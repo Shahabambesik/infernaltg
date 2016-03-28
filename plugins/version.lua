bb7c9ba654b0221d9407b831b6f37da25d52ec13
Shahab <safaeishahab@yahoo.com>
2016-03-28 21:55:16 +0430

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,25 @@
+do
+
+function run(msg, matches)
+  return [[👑íղƒҽɾղɑӀ👑 #ＴＧ
+An advanced administration bot based on pluigns(lua)
+
+Admins
+
+arshyaw [developer/master]
+ShahabHiDDeN [developer/etc]
+
+Have nice day with out spam😏
+https://telegram.me/InfernalTG]]
+end
+
+return {
+  description = "infernaltg", 
+  usage = "!infernaltg: Shows bot version",
+  patterns = {
+    "^[#!/]infernaltg$"
+  }, 
+  run = run 
+}
+
+end
